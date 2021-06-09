8eb38ce1d1a5f4244a6654a299cf413cbcc19150
SyedAfrozPasha <afroz.pasha26@gmail.com>
2021-06-09 12:45:49 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,23 +9,26 @@ import './style.css';
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import UtilityViewer from './Components/UtilityViewer';
-import JSONViewer from './Components/JSONViewer';
+
+const UtilityViewer = React.lazy(() => import('./Components/UtilityViewer'));
+const JSONViewer = React.lazy(() => import('./Components/JSONViewer'));
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Router>
         <Header />
-        <Switch>
-          <Route exact path="/string-utility-tool">
-            <UtilityViewer />
-          </Route>
-          <Route exact path="/json-viewer">
-            <JSONViewer />
-          </Route>
-          <Redirect exact from="/" to="string-utility-tool" />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/string-utility-tool">
+              <UtilityViewer />
+            </Route>
+            <Route exact path="/json-viewer">
+              <JSONViewer />
+            </Route>
+            <Redirect exact from="/" to="string-utility-tool" />
+          </Switch>
+        </Suspense>
       </Router>
       <Footer />
     </div>
